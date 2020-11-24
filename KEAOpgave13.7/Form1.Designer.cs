d576588585b7e020c6669a608d92c380048e9917
Alex Skram Jensen <asj@tutamail.com>
2020-11-24 09:29:36 +0100

    Add project files.

---
+++ KEAOpgave13.7/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace KEAOpgave13._7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonTable = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(88, 81);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(139, 260);
+            this.listBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(275, 133);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(309, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Which multiplication table should be calculated?";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(278, 153);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // buttonTable
+            // 
+            this.buttonTable.Location = new System.Drawing.Point(278, 235);
+            this.buttonTable.Name = "buttonTable";
+            this.buttonTable.Size = new System.Drawing.Size(100, 33);
+            this.buttonTable.TabIndex = 3;
+            this.buttonTable.Text = "Show Table";
+            this.buttonTable.UseVisualStyleBackColor = true;
+            this.buttonTable.Click += new System.EventHandler(this.buttonTable_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(385, 164);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 17);
+            this.label2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonTable);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonTable;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ KEAOpgave13.7/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KEAOpgave13._7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        class Table
+        {
+            // Fields
+            private int factor = 0;
+            private int[] tables = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+
+            // Properties
+            public int Factor
+            {
+                get
+                {
+                    return factor;
+                }
+                set
+                {
+                    if (value > 0)
+                        factor = value;
+                    else
+                        factor = 10;
+                }
+            }
+
+            public Table() { }
+
+            public int Multiplication(int n)
+            {
+                int result = 0;
+
+                return result = Factor * n;
+
+            }
+        }
+        private void buttonTable_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            Table myTable = new Table();
+            try
+            {
+                myTable.Factor = Convert.ToInt32(textBox1.Text);
+            }
+            catch
+            {
+                label2.Text = "Numbers only!    ";
+            }
+            int n = 1;
+            while (n <= 10)
+            {
+                listBox1.Items.Add(myTable.Multiplication(n));
+                n++;
+            }
+            
+        }
+    }
+}
